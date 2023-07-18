ba5104d7267221402bd7a4ec75e758cb81f046ec
Olga <Mamillaris21@gmail.com>
2023-07-18 12:31:09 +0400

    lesson 4

--- dist/app.js
+++ dist/app.js
@@ -1,67 +1,45 @@
 "use strict";
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
-let age;
-age = 50;
-let namee;
-namee = 'Leo';
-let toggle;
-toggle = true;
-let empty;
-empty = null;
-let notInitialize;
-notInitialize = undefined;
-let callback;
-callback = (a) => { return 100 + a; };
-let anything;
-anything = -20;
-anything = 'Text';
-anything = {};
-let some;
-some = 'Text';
-let str;
-if (typeof some === "string") {
-    str = some;
-}
-let person;
-person = ['Max', 21];
-var Load;
-(function (Load) {
-    Load[Load["LOADING"] = 0] = "LOADING";
-    Load[Load["READY"] = 1] = "READY";
-})(Load || (Load = {}));
-;
-let x;
-let y;
-function showMessage(message) {
-    console.log(message);
-}
-function calc(num1, num2) {
-    return num1 + num2;
-}
-function customError() {
-    throw new Error('Error');
-}
-const page1 = {
-    title: 'The awesome page',
-    likes: 100,
-    accounts: ['Max', 'Anton', 'Nikita'],
-    status: 'open',
-    details: {
-        createAt: new Date('2021-01-01'),
-        updateAt: new Date('2021-05-01'),
+class Key {
+    constructor() {
+        this.signature = Math.random();
     }
-};
-const page2 = {
-    title: 'Python or Js',
-    likes: 5,
-    accounts: ['Alex'],
-    status: 'close',
-};
+    getSignature() {
+        return this.signature;
+    }
+}
+class Person {
+    constructor(key) {
+        this.key = key;
+    }
+    getKey() {
+        return this.key;
+    }
+}
+class House {
+    constructor(key) {
+        this.key = key;
+        this.doorOpen = false;
+        this.tenants = [];
+    }
+    comeIn(person) {
+        if (!this.doorOpen) {
+            throw new Error('Door is close');
+        }
+        this.tenants.push(person);
+        console.log('Person inside');
+    }
+}
+class MyHouse extends House {
+    openDoor(key) {
+        if (key.getSignature !== this.key.getSignature) {
+            throw new Error('Key to another door');
+        }
+        this.doorOpen = true;
+    }
+}
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+house.openDoor(person.getKey());
+house.comeIn(person);
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,89 +1,67 @@
-const button = document.querySelector("button")!;
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+class Key {
+  private signature: number;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
-
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
-
-let age: number;
-age = 50;
-let namee: string;
-namee = 'Leo';
-let toggle: boolean;
-toggle = true;
-let empty: null;
-empty = null;
-let notInitialize: undefined;
-notInitialize = undefined;
-let callback: (num:number) =>{};
-callback = (a) => { return 100 + a };
-
-let anything: any;
-anything = -20;
-anything = 'Text';
-anything = {};
-
-let some:unknown;
-some = 'Text';
-
-let str: string;
-if(typeof some === "string"){
-  str = some;
-}
-
-let person: [string, number];
-person = ['Max', 21];
-
-enum Load {'LOADING', 'READY'};
-
-let x: string | number;
-
-let y:  'enable' | 'disable';
-
-
-function showMessage(message: string) :void {
-  console.log(message);
-}
-
-
-function calc(num1:number, num2:number) :number{
-  return num1 + num2;
-}
-
-function customError(): never {
-  throw new Error('Error');
-}
-
-type Page = {
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: 'open' | 'close',
-  details?: {
-    createAt: Date,
-    updateAt?: Date
+  constructor(){
+      this.signature = Math.random();
+  }
+  getSignature (): number {
+      return this.signature;
   }
 }
 
-const page1: Page = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
+class Person {
+  constructor(private key: Key){
+  }
+  getKey(): Key{
+    return this.key
   }
 }
 
-const page2: Page = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
+abstract class House {
+  protected doorOpen = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key){
+  }
+
+  comeIn(person: Person):void {
+    if(!this.doorOpen){
+      throw new Error('Door is close')
+    }
+    this.tenants.push(person);
+    console.log('Person inside')
+  }
+
+  abstract openDoor (key: Key): void;
 }
+
+class MyHouse extends House {
+  openDoor (key:Key){
+      if(key.getSignature!==this.key.getSignature){
+        throw new Error('Key to another door');
+      }
+      this.doorOpen = true;
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+// const buttonEl = document.querySelector("button")!;
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+// // function add(num1: number, num2: number) {
+// //   return num1 + num2;
+// // }
+
+// buttonEl.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
